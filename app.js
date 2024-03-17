a0f2a7f64354b2f9276d0cb09edebb0788a76b17
icarante <icarus.wedekind23@student.cart.org>
2024-03-16 22:41:18 -0700

    GOT MOST OF THE JS I NEED TO WORK WORKING!!!!

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ getId('incform').addEventListener('submit', function firstIncome(input) {
     var salary = parseFloat(getId('salary').value);
     inputArray.push(parseFloat(salary));
 
-    var addinc = parseFloat(getId('addinc').value);
+    var addinc = parseFloat(getId('addInc').value);
     inputArray.push(parseFloat(addinc));
 
     //iterate through array to get the total amount of income
@@ -146,12 +146,12 @@ getId('incform').addEventListener('submit', function firstIncome(input) {
     let total = Math.round(finSum * .3265);
 
     //have the 50/30/20 appear where it needs to on the page
-    getId('need').innerText = `$${Math.round(total * .5)}`;
-    getId('want').innerText = `$${Math.round(total * .3)}`;
-    getId('savings').innerText = `$${Math.round(total * .2)}`;
-    getId('necessity').innerText = `$${Math.round(total * .5)}`
-    getId('life').innerText = `$${Math.round(total * .3)}`;
-    getId('savi').innerText = `$${Math.round(total * .2)}`;
+    getId('need').innerText = `$${Math.round(total * .5).toLocaleString()}`;
+    getId('want').innerText = `$${Math.round(total * .3).toLocaleString()}`;
+    getId('savings').innerText = `$${Math.round(total * .2).toLocaleString()}`;
+    getId('necessity').innerText = `$${Math.round(total * .5).toLocaleString()}`
+    getId('life').innerText = `$${Math.round(total * .3).toLocaleString()}`;
+    getId('savi').innerText = `$${Math.round(total * .2).toLocaleString()}`;
 
     return total;
 });
@@ -163,21 +163,19 @@ getId('incform').addEventListener('submit', function firstIncome(input) {
 getId(`form1`).addEventListener('submit', function (input) {
     input.preventDefault();
 
-     
     //collect want values and push them into an returnable array for future use
-    function wants() {
         var wantArray1 = [];
 
-        var ogtrans = document.getId('trans1').value;
+        var ogtrans = getId('trans').value;
         wantArray1.push(parseFloat(ogtrans));
 
-        var oghob = parseFloat(document.getId('hobb1').value);
+        var oghob = parseFloat(getId('hobb').value);
         wantArray1.push(parseFloat(oghob));
 
-        var ogout = parseFloat(document.getId('out1').value);
+        var ogout = parseFloat(getId('out').value);
         wantArray1.push(parseFloat(ogout));
 
-        var ogfood = parseFloat(document.getId('food1').value);
+        var ogfood = parseFloat(getId('food').value);
         wantArray1.push(parseFloat(ogfood));
 
         for (i = 0; i < wantArray1; i++) {
@@ -186,20 +184,16 @@ getId(`form1`).addEventListener('submit', function (input) {
             }
         }
 
-        return wantArray1;
-    }
-
     //same as above for savings values
-    function sav() {
         var savArray1 = [];
 
-        var ogsav = parseFloat(document.getId('sav1').value);
+        var ogsav = parseFloat(getId('sav').value);
         savArray1.push(parseFloat(ogsav));
 
-        var ogretir = parseFloat(document.getId('retir1').value);
+        var ogretir = parseFloat(getId('retir').value);
         savArray1.push(parseFloat(ogretir));
 
-        var ogextra = parseFloat(document.getId('extra1').value);
+        var ogextra = parseFloat(getId('extra').value);
         savArray1.push(parseFloat(ogextra));
 
         for (i = 0; i < savArray1; i++) {
@@ -208,278 +202,82 @@ getId(`form1`).addEventListener('submit', function (input) {
             }
         }
 
-        return savArray1;
-    }
-
     //same as above for needs values
-    function needs() {
 
-        var needArray1 = [];
 
-        var oghousing = parseFloat(document.getId('housing1').value);
-        needArray1.push(parseFloat(oghousing));
+    var needArray1 = [];
 
-        var ogutil = parseFloat(document.getId('util1').value);
-        needArray1.push(parseFloat(ogutil));
+    var oghousing = parseFloat(getId('housing').value);
+    needArray1.push(parseFloat(oghousing));
 
-        var oginsur = parseFloat(document.getId('insur1').value);
-        needArray1.push(parseFloat(oginsur));
+    var ogutil = parseFloat(getId('util').value);
+    needArray1.push(parseFloat(ogutil));
 
-        var ogdebt = parseFloat(document.getId('debt1').value);
-        needArray1.push(parseFloat(ogdebt));
+    var oginsur = parseFloat(getId('insur').value);
+    needArray1.push(parseFloat(oginsur));
 
-        for (i = 0; i < needArray1; i++) {
-            if (isNaN(needArray1[i])) {
-                needArray1[i] = 0;
-            }
-        }
+    var ogdebt = parseFloat(getId('debt').value);
+    needArray1.push(parseFloat(ogdebt));
 
-        return needArray1;
-    }
-
-    //total all the arrays individually so they can be added together
-    function needsTot() {
-
-        let needSum = 0;
-
-        for (i = 0; i < needs().length; i++) {
-            if (!isNaN(needs()[i])) {
-                console.log(needs()[i]);
-                needSum += needs()[i];
-
-            }
-        }
-        return needSum;
-    }
-
-    function wantsTot() {
-        let wantSum = 0;
-
-        for (i = 0; i < wants().length; i++) {
-            if (!isNaN(wants()[i])) {
-                console.log(wants()[i]);
-                wantSum += wants()[i];
-            }
-        }
-        return wantSum;
-    }
-
-    function savTot() {
-
-        let savSum = 0;
-
-        for (i = 0; i < sav().length; i++) {
-            if (!isNaN(sav()[i])) {
-                console.log(sav()[i]);
-                savSum += sav()[i];
-
-            }
+    for (i = 0; i < needArray1; i++) {
+        if (isNaN(needArray1[i])) {
+            needArray1[i] = 0;
         }
     }
+
 
     //add the results together to be used for later operation
-    let nonBudgSums = wantsTot() + needsTot() + savTot();
+    let nonBudgSums = { wants: wantArray1, needs: needArray1, savings: savArray1 };
+    // Pull arrays from object
+
+    let wants = nonBudgSums.wants;
+    let needs = nonBudgSums.needs;
+    let savs = nonBudgSums.savings;
+
+    // add arrays together to get a total
+
+    let necSum = 0;
+
+    for (i = 0; i < wants.length; i++) {
+        if (!isNaN(wants[i])) {
+            console.log(wants[i]);
+            necSum += wants[i];
+
+        }
+    }
+
+    let needSum = 0;
+
+    for (i = 0; i < needs.length; i++) {
+        if (!isNaN(needs[i])) {
+            console.log(needs[i]);
+            needSum += needs[i];
+
+        }
+    }
+
+    let saviSum = 0;
+
+    for (i = 0; i < savs.length; i++) {
+        if (!isNaN(savs[i])) {
+            console.log(savs[i]);
+            saviSum += savs[i];
+
+        }
+    }
+
+    let compSum = necSum + needSum + saviSum;
+
+    if(compSum > incomeInput){
+        getId('ovUn').innerText = `$${Math.round(parseInt(compSum-incomeInput)).toLocaleString()} under`;
+    }
+    else {
+        getId('ovUn').innerText = `$${Math.round(parseInt(compSum-incomeInput)).toLocaleString()} over`;
+    }
+
 
     return nonBudgSums;
 });
-
-
-//grab inputs from the second form
-getId(`form2`).addEventListener('submit', function budgetPlan(input) {
-    input.preventDefault();
-
-    //make a function with a return i can use later
-    function newWants() {
-        var newWantArray1 = [];
-
-        var newtrans = parseFloat(document.getId('trans2').value);
-        newWantArray1.push(parseFloat(newtrans));
-
-        var newhob = parseFloat(document.getId('hobb2').value);
-        newWantArray1.push(parseFloat(newhob));
-
-        var newout = parseFloat(document.getId('out2').value);
-        newWantArray1.push(parseFloat(newout));
-
-        var newfood = parseFloat(document.getId('food2').value);
-        newWantArray1.push(parseFloat(newfood));
-
-        return newWantArray1;
-    };
-
-    //same as above
-    function newSavs() {
-
-        var newSavArray1 = [];
-
-        var newsav = parseFloat(document.getId('sav2').value);
-        newSavArray1.push(parseFloat(newsav));
-
-        var newretir = parseFloat(document.getId('retir2').value);
-        newSavArray1.push(parseFloat(newretir));
-
-        var newextra = parseFloat(document.getId('extra2').value);
-        newSavArray1.push(parseFloat(newextra));
-
-        return newSavArray1;
-    };
-
-    // same as above
-    function newNeeds() {
-
-        var newNeedArray1 = [];
-
-        var newhousing = parseFloat(document.getId('housing2').value);
-        newNeedArray1.push(parseFloat(newhousing));
-
-        var newutil = parseFloat(document.getId('util2').value);
-        newNeedArray1.push(parseFloat(newutil));
-
-        var newinsur = parseFloat(document.getId('insur2').value);
-        newNeedArray1.push(parseFloat(newinsur));
-
-        var newdebt = parseFloat(document.getId('deb2').value);
-        newNeedArray1.push(parseFloat(newdebt));
-
-        return newNeedArray1;
-    }
-
-    //sum up the previously made arrays for total use
-    function newWantsTot() {
-        let newWantSum = 0;
-
-        for (i = 0; i < newWants().length; i++) {
-            if (!isNaN(newWants()[i])) {
-                console.log(newWants()[i]);
-                newWantSum += newWants()[i];
-
-            }
-        }
-
-        return newWantSum;
-    };
-
-    function newNesTot() {
-        let newNeedSum = 0;
-        for (i = 0; i < newNeeds().length; i++) {
-            if (!isNaN(newNeeds()[i])) {
-                console.log(newNeeds()[i]);
-                newNeedSum += newNeeds()[i];
-
-            }
-        }
-        return newNeedSum;
-    }
-
-    function newSavTot() {
-        let newSavSum = 0;
-
-        for (i = 0; i < newSavs().length; i++) {
-            if (!isNaN(newSavs()[i])) {
-                console.log(newSavs()[i]);
-                newSavSum += newSavs()[i];
-
-            }
-        }
-        return newSavSum;
-    }
-
-
-    let budgSum = newNesTot() + newSavTot() + newWantsTot();
-
-    return budgSum;
-});
-
-//save the returns as accessible variables
-var ogneeds = need();
-var ogwants = want();
-var ogsaving = sav();
-
-// create an array of all the original inputs
-function ogArray() {
-    let ogArray = 0;
-    for (i = 0; i < ogneeds.length; i++) {
-        ogArray += ogneeds[i];
-    }
-    for (i = 0; i < ogwants.length; i++) {
-        ogArray += ogwants[i];
-    }
-    for (i = 0; i < ogsaving.length; i++) {
-        ogArray += ogsaving[i];
-    }
-    return ogArray;
-}
-
-
-//^^ arrays pulled from form 1
-
-
-//do the same thing as above but with the new budget inputs
-var newWants = newWants();
-var newNeeds = newNeeds();
-var newSav = newSav();
-
-function newArray() {
-    let newArray = 0;
-    for (i = 0; i < newNeeds.length; i++) {
-        newArray += newNeeds[i];
-    }
-    for (i = 0; i < newWants.length; i++) {
-        newArray += newWants[i];
-    }
-    for (i = 0; i < newSav.length; i++) {
-        newArray += newSav[i];
-    }
-    return newArray;
-}
-// ^^ arrays pulled from form 2
-
-//save the results of the income stuff as well
-var budgTotal = budgetPlan();
-var startPoint = firstIncome();
-
-// display whether you're over or under budget
-function budgDetermine() {
-    if (Math.round(startPoint - budgTotal) > 0) {
-        getId('ovUn').innerText = ` $${Math.round(startPoint - budgTotal)} under `;
-    }
-    else if (Math.round(startPoint - budgTotal) === 0) {
-        getId('ovUn').innerText = ` exactly on `;
-    }
-    else {
-        getId('ovUn'.innerText = ` $${Math.round(startPoint - budgTotal)} over`);
-    }
-};
-
-//display 30% of the income for housing stuff
-function forHouse() {
-    getId('houBudg').innerText = ` $${Math.round(startPoint * .3)}`;
-}
-
-//iterate through both arrays to change the innertext accordingly for the first category
-function oldBudgReplace() {
-    let oldlabels = [getId('curhou'), getId('curut'), getId('curin'), getId('curdeb'), getId('curtrans'), getId('curhob'), getId('curfood'), getId('cursav'), getId('currer'), getId('curcase')];
-    for (i = 0; i < oldlabels.length; i++) {
-            oldlabels[i].innerText = ogArray()[i];
-    }
-}
-
-//iterate through both arrays to change the innertext accordingly for the second category
-function newBudgReplace(){
-    let newlabels = [getId('newhou'), getId('newut'), getId('newin'), getId('newdeb'), getId('newtrans'), getId('newhob'), getId('newfood'), getId('newsav'), getId('newrer'), getId('newcase')];
-    for (i = 0; i < newlabels.length; i++) {
-        newlabels[i].innerText = newArray()[i];
-}
-}
-
-//add the inputs and stuff only when the summary button is clicked
-getId('summaryBtn').addEventListener('click', function (input) {
-    input.preventDefault();
-    budgDetermine();
-    forHouse();
-    oldBudgReplace();
-    newBudgReplace();
-})
 
 
 // ^^ pie stuff
